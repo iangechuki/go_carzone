d81a80289663065f9185d63c15191a66eb5e8cb4
Ian Gechuki <iangechuki@gmail.com>
2025-05-13 22:53:09 +0300

    refactor: fixed bugs on car and engine store

--- store/engine/engine.go
+++ store/engine/engine.go
@@ -74,7 +74,7 @@ func (s *EngineStore) CreateEngine(ctx context.Context,engineReq *models.EngineR
 	}()
 	engineID := uuid.New()
 
-	_,err = tx.ExecContext(ctx,"INSERT INTO engine (id,displacement,no_of_cylinders,car_range,created_at,updated_at) VALUES ($1,$2,$3,$4,$5,$6)",
+	_,err = tx.ExecContext(ctx,"INSERT INTO engine (id,displacement,no_of_cylinders,car_range) VALUES ($1,$2,$3,$4)",
 		engineID,
 		engineReq.Displacement,
 		engineReq.NoOfCylinders,
@@ -113,10 +113,17 @@ func (s *EngineStore) UpdateEngine(ctx context.Context,id string,engineReq *mode
 			}
 		}
 	}()
-	results,err := tx.ExecContext(ctx,"UPDATE engine SET displacement = $1,no_of_cylinders = $2,car_range = $3 WHERE id = $4",
+	results,err := tx.ExecContext(
+		ctx,
+		`UPDATE engine 
+		SET displacement = $1,
+			no_of_cylinders = $2,
+			car_range = $3
+		WHERE id = $4`,
 		engineReq.Displacement,
 		engineReq.NoOfCylinders,
 		engineReq.CarRange,
+		engineID,
 	)
 	if err != nil {
 		return models.Engine{},err
@@ -138,24 +145,27 @@ func (s *EngineStore) UpdateEngine(ctx context.Context,id string,engineReq *mode
 }
 
 func (s *EngineStore) DeleteEngine(ctx context.Context,id string) (models.Engine,error) {
-	var engine models.Engine
+	engineID,err := uuid.Parse(id)
+	if err != nil {
+		return models.Engine{},fmt.Errorf("invalid engine ID: %w",err)
+	}
 	tx,err := s.db.BeginTx(ctx, nil)
 	if err != nil {
 		return models.Engine{},err
 	}
-	defer func(){
-		if err != nil {
-			if rbErr := tx.Rollback(); rbErr != nil {
-				fmt.Printf("could not rollback transaction: %v\n", rbErr)
-			}
-		}else {
-			if cmErr := tx.Commit(); cmErr != nil {
-				fmt.Printf("could not commit transaction: %v\n", cmErr)
-			}
-		}
-	}()
+	defer func() {
+        if err != nil {
+            tx.Rollback()
+        } else {
+            err = tx.Commit()
+        }
+    }()
+	var engine models.Engine
 
-	err = tx.QueryRowContext(ctx,"SELECT id,displacement,no_of_cylinders,car_range FROM engine WHERE id = $1",id).Scan(
+	err = tx.QueryRowContext(
+		ctx,
+		`SELECT id,displacement,no_of_cylinders,car_range 
+		FROM engine WHERE id = $1`,engineID).Scan(
 		&engine.EngineID,
 		&engine.Displacement,
 		&engine.NoOfCylinders,
@@ -169,7 +179,7 @@ func (s *EngineStore) DeleteEngine(ctx context.Context,id string) (models.Engine
 			return models.Engine{},err
 		}
 	}
-	result,err := tx.ExecContext(ctx,"DELETE FROM engine WHERE id = $1",id)
+	result,err := tx.ExecContext(ctx,"DELETE FROM engine WHERE id = $1",engineID)
 	if err != nil {
 		return models.Engine{},err
 	}
